0be92f5f3c35fad5517b0fb1fcdeabe0dfabaccb
MUHAMMADUMAIRSADIQ-CODE1 <ghulamrabia0048@gmail.com>
2025-12-04 12:39:22 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function ageCalculator() {
+    var input = document.getElementById("input").value;
+
+    var ageDate = new Date(input);
+    var nowDate = new Date();
+    var diff = nowDate - ageDate;
+
+    var years = (diff / (1000 * 60 * 60 * 24 * 30.44 * 12)).toFixed(2);
+    
+    var months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30.44));
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(diff / (1000 * 60 * 60));
+    var seconds = Math.floor(diff / 1000);
+
+    document.getElementById("result").innerHTML = `
+        <p><b>Years:</b> ${years}</p>
+        <p><b>Months:</b> ${months}</p>
+        <p><b>Days:</b> ${days}</p>
+        <p><b>Hours:</b> ${hours}</p>
+        <p><b>Seconds:</b> ${seconds}</p>
+    `;
+}

ee293d749c835e54bdb64c33e1097d47ace89943
MUHAMMADUMAIRSADIQ-CODE1 <ghulamrabia0048@gmail.com>
2025-12-04 13:54:26 +0500

    first commit

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ function ageCalculator() {
     var nowDate = new Date();
     var diff = nowDate - ageDate;
 
-    var years = (diff / (1000 * 60 * 60 * 24 * 30.44 * 12)).toFixed(2);
+    var years = Math.floor((diff / (1000 * 60 * 60 * 24 * 30.44 * 12)));
     
-    var months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30.44));
-    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(diff / (1000 * 60 * 60));
-    var seconds = Math.floor(diff / 1000);
+    var months = Math.floor((diff / (1000 * 60 * 60 * 24 * 30.44))%12);
+    var days = Math.floor((diff / (1000 * 60 * 60 * 24))%30.44);
+    var hours = Math.floor((diff / (1000 * 60 * 60))%24);
+    var minutes = Math.floor((diff / (1000 * 60 ))%60);
+    var seconds = Math.floor((diff / 1000))%60;
 
     document.getElementById("result").innerHTML = `
         <p><b>Years:</b> ${years}</p>
         <p><b>Months:</b> ${months}</p>
         <p><b>Days:</b> ${days}</p>
         <p><b>Hours:</b> ${hours}</p>
+        <p><b>Minutes:</b> ${minutes}</p>
         <p><b>Seconds:</b> ${seconds}</p>
     `;
 }
